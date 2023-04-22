5739d9b75059730266d0c1b07a813f9fc62e4df1
Vladlen <ilusenkovladlen@gmail.com>
2023-04-22 19:13:00 +0300

    Implemented Bank synchronization

--- src/bank/TransferThread.java
+++ src/bank/TransferThread.java
@@ -1,9 +1,9 @@
 package bank;
 
 class TransferThread extends Thread {
-    private Bank bank;
-    private int fromAccount;
-    private int maxAmount;
+    private final Bank bank;
+    private final int fromAccount;
+    private final int maxAmount;
     private static final int REPS = 1000;
 
     public TransferThread(Bank b, int from, int max) {
@@ -17,7 +17,7 @@ class TransferThread extends Thread {
             for (int i = 0; i < REPS; i++) {
                 int toAccount = (int) (bank.size() * Math.random());
                 int amount = (int) (maxAmount * Math.random() / REPS);
-                bank.transfer(fromAccount, toAccount, amount);
+                bank.transferSynchronized(fromAccount, toAccount, amount);
             }
         }
     }
