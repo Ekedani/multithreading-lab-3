2fdad1b74995586edc3c29fad636bf99d7cb344b
Vladlen <ilusenkovladlen@gmail.com>
2023-04-24 13:01:44 +0300

    Added original consumer-producer

---
+++ src/guardmeth/Drop.java
@@ -0,0 +1,28 @@
+package guardmeth;
+
+public class Drop {
+    private String message;
+    private boolean empty = true;
+
+    public synchronized String take() {
+        while (empty) {
+            try {
+                wait();
+            } catch (InterruptedException ignored) {}
+        }
+        empty = true;
+        notifyAll();
+        return message;
+    }
+
+    public synchronized void put(String message) {
+        while (!empty) {
+            try {
+                wait();
+            } catch (InterruptedException ignored) {}
+        }
+        empty = false;
+        this.message = message;
+        notifyAll();
+    }
+}
